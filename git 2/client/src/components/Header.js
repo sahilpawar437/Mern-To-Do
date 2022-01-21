273cc9eb60c030421d933e8c4d4b32ff4ccbfb77
sahilpawar437 <69265832+sahilpawar437@users.noreply.github.com>
2022-01-21 11:23:24 +0530

    Add files via upload

---
+++ git 2/client/src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Header() {
+    return (
+        <div className='card bg-dark text-center text-light rounded-0'>
+            <h1 className="display-4">
+                <span className="text-light mr-3">To-Do List TechHut Assignment</span>
+            </h1>
+        </div>
+    )
+}
